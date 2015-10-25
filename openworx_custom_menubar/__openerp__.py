5747a29b9783e7d56cac255cee0fba906c022a3c
Mario Gielissen <mario@gielissen.org>
2015-10-25 16:41:51 -0400

    First commit

---
+++ openworx_custom_menubar/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name' : 'Custom Menubar makeover',
+    'version' : '1.0.0',
+    'author' : 'Openworx',
+    'category' : 'Tools',
+    'website' : 'http://www.openworx.nl',
+    'description': """
+Give Odoo 8.0 menubar a enterprise makeover.
+    """,
+    'depends' : ['base'],
+    'data':[
+        'views.xml',
+        ],
+    'installable': True
+}
