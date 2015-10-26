c3992c5f1999ab84fad08dbe7ed254a982910222
Mario Gielissen <mario@gielissen.org>
2015-10-26 18:21:57 -0400

    First commit

---
+++ openworx_backend_theme/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name' : 'Openworx Backend Theme',
+    'version' : '0.1',
+    'author' : 'Openworx',
+    'category' : 'Tools',
+    'website' : 'http://www.openworx.nl',
+    'description': """
+Give Odoo 8.0 backend a v9 enterprise look&feel.
+    """,
+    'depends' : ['base'],
+    'data':[
+        'views.xml',
+        ],
+    'installable': True
+}
