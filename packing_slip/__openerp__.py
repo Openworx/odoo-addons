f4266fae854812bf645d7114ce098d9dd532b17f
Mario Gielissen <mario@gielissen.org>
2016-02-08 15:32:02 +0100

    first commit

---
+++ packing_slip/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Packing Slip',
+    'version': '0.0.1',
+    'author': 'Openworx',
+    'category': 'Sales',
+    'summary': 'Packing Slip Report',
+    'description': """
+    This module adds a print option in the order form for a packing slip.
+    """,
+    'website': 'http://www.openworx.nl',
+    'images': [],
+    'depends': ['base', 'sale'],
+    'sequence': 18,
+    'data': [
+        'report_packing_slip.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
