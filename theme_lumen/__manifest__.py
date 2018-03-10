5a5bebfc4e476913e89d4aa2edb6e5d822720428
root <root@odoo11-themes.lan>
2018-03-10 18:30:44 -0500

    First Commit

---
+++ theme_lumen/__manifest__.py
@@ -0,0 +1,33 @@
+{
+    'name': "Bootswatch Lumen",
+
+    'summary': """
+    Bootswatch Lumen""",
+
+    'description': """
+    Bootswatch Lumen Theme https://bootswatch.com/lumen/""",
+
+    'author': "Openworx",
+    'website': "https://www.openworx.nl",
+
+    'category': 'Theme',
+    'version': '1.0',
+
+    'images':[
+        'images/lumen-thumbnail.png'
+    ],
+
+    'depends': [
+        'website'
+    ],
+    'data': [
+	'data/theme_lumen_data.xml',
+        'views/bootswatch_lumen.xml'
+    ],
+
+    'demo': [
+    ],
+
+    'tests': [
+    ],
+}
