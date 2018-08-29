162de8878b77d246b8c9885333e8de7f9e718cc0
root <root@docker3.openworx.nl>
2018-08-29 15:30:13 +0200

    Login Portal Page added

---
+++ login_page/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'Website Portal Login Page',
+    'summary': 'Make Odoo Website loginpage like fullscreen portal',
+    'version': '11.0.1.0',
+    'category': 'Website',
+    'summary': """
+Make Odoo Website loginpage like fullscreen portal
+""",
+    'author': "Openworx",
+    'website': 'http://www.openworx.nl',
+    'license': 'LGPL-3',
+    'images': ['images/loginpage.png'],
+    'depends': [
+	'website',
+    ],
+    'data': [
+        'templates/portal_login_page.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
