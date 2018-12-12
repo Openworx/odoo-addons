978b8bc647bfc272361d63bf1e4d692fa3ee12b1
root <root@docker2.openworx.nl>
2018-12-12 23:17:00 +0100

    Account Asset and Budget backported from Odoo 12.0 CE version 20180927

---
+++ ow_account_asset/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Assets Management',
+    'author': 'Odoo SA, Openworx',
+    'depends': ['account'],
+    'description': """
+Assets management
+=================
+Manage assets owned by a company or a person.
+Keeps track of depreciations, and creates corresponding journal entries.
+
+This module is backported from Odoo version 20180927
+    """,
+    'summary': 'Assets Management backported to Odoo 12.0 CE',
+    'category': 'Accounting',
+    'sequence': 32,
+    'data': [
+        'security/account_asset_security.xml',
+        'security/ir.model.access.csv',
+        'wizard/asset_depreciation_confirmation_wizard_views.xml',
+        'wizard/asset_modify_views.xml',
+        'views/account_asset_views.xml',
+        'views/account_invoice_views.xml',
+        'views/account_asset_templates.xml',
+        'views/product_views.xml',
+        'views/res_config_settings_views.xml',
+        'report/account_asset_report_views.xml',
+        'data/account_asset_data.xml',
+    ],
+    'qweb': [
+        "static/src/xml/account_asset_template.xml",
+    ],
+}
