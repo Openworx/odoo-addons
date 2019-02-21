5ef64d48d38cf1d66b3de4af45a86bf022563273
root <root@docker2.openworx.nl>
2019-02-21 21:27:02 +0100

    TimeSheet Today Filter

---
+++ hr_timesheet_today/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "HR Timesheet Filter Today",
+    "summary": "Add Filter Today",
+    "version": "11.0",
+    "author": "Openworx",
+    "website": "https://www.openworx.nl",
+    "category": "Human Resources",
+    "depends": ['hr_timesheet'],
+    "data": [
+        'views/timesheet_views.xml',
+    ],
+    "license": "LGPL-3",
+    'images': ['images/today.png'],
+    "installable": True,
+    "application": False,
+}
