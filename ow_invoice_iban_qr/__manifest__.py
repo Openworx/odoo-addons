87978d88a3fd3c4cf4d75841cdeb90096eef793c
Mario Gielissen <mario@gielissen.org>
2019-03-04 13:05:04 +0100

    Update __manifest__.py

--- ow_invoice_iban_qr/__manifest__.py
+++ ow_invoice_iban_qr/__manifest__.py
@@ -3,8 +3,8 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    "name": "IBAN QR Code on invoice",
-    "summary": "Add IBAN QR Code on invoice for scanning in mobile banking apps",
+    "name": "IBAN QR Code on Invoice",
+    "summary": "Add IBAN QR Code on Invoice for scanning in mobile banking apps",
     "version": "0.1",
     "author": "Openworx",
     "website": "https://www.openworx.nl",
