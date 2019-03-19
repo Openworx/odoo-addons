72602c0d3a5f7488ee5790c7d1a59a04f55b9885
root <root@dock1.openworx.nl>
2019-03-19 14:32:06 +0100

    Odoo Message Notifier app

---
+++ ow_notify/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Openworx
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Openworx Notify",
+    "summary": "Sound and push notifications on new messages.",
+    "version": "12.0.0.1",
+    "category": "Base",
+    "website": "http://www.openworx.nl",
+    "description": """
+    Sound and push notifications on new messages.
+    """,
+    'images':[
+    ],
+    "author": "Openworx",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+	'mail',
+    ],
+    "data": [
+        'views/assets.xml',
+    ],
+
+}
