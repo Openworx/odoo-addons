cdc51570718dfc02aedc00fc06f8e52812fc763f
root <root@docker2.openworx.nl>
2019-04-04 16:14:15 +0200

    User Contract Portal added

---
+++ ow_contract_portal/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Openworx
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Contracts Portal',
+    'category': 'Contract Management',
+    'summary': 'View contracts in user portal',
+    'version': '1.0',
+	'license': 'LGPL-3',
+    'author': 'Openworx',
+    'description': """
+        View contracts in user portal
+    """,
+	'images':['images/screenshot.png'],
+    'website': 'http://www.openworx.nl',
+    'depends': [
+        'website',
+        'contract',
+    ],
+    'data': [
+        'views/templates.xml',
+        'security/portal_security.xml',
+		'security/ir.model.access.csv'
+    ],
+    'installable': True,
+}
