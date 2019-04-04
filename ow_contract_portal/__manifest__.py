1833a3ad780c93e359ffd3124cfe688d0f228946
Mario Gielissen <mario@gielissen.org>
2019-04-04 16:25:49 +0200

    Update __manifest__.py

--- ow_contract_portal/__manifest__.py
+++ ow_contract_portal/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 Openworx
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Contracts Portal',
