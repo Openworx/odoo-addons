290117f78ea7b0c1a45fc12a2fe60f6305a3bdef
Mario Gielissen <mario@gielissen.org>
2020-08-31 14:21:19 +0200

    Update __manifest__.py

--- kix-code/__manifest__.py
+++ kix-code/__manifest__.py
@@ -5,7 +5,7 @@
     'version': '10.0.1',
     'author': 'Openworx',
     'category': 'Extra Tools',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'summary': 'PostNL Address barcode on invoice',
     'description': """
 PostNL maakt bij de sortering van post gebruik van een streepjescode: de KIX (KlantIndeX). Deze code voegt u toe aan de adresgegevens. De KIX bevat de gegevens die wij nodig hebben om uw post met de modernste sorteermachines te verwerken. Door de KIX bij het adres af te drukken helpt u ons om de kwaliteit van de dienstverlening te verbeteren.
