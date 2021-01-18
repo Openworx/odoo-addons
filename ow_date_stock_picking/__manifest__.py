801880ef55b49eaa5cedba88aa3031fef2c1717d
Mario <mario@gielissen.org>
2021-01-18 11:31:39 +0100

    Edit date for stock picking

---
+++ ow_date_stock_picking/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Openworx
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Edit Date in Stock Picking',
+    'summary': 'Make date fields editable in Stock Picking when done',
+    'version': '1.0',
+    'category': 'Warehouse',
+    'description': """
+    Make date fields editable in Stock Picking when done
+""",
+    'author': "Openworx",
+    'website': 'https://www.openworx.nl',
+    'license': 'LGPL-3',
+    'depends': [
+	'stock',
+    ],
+    'data': [
+    ],
+    'images': ['images/image.png'],
+    'installable': True,
+    'application': False,
+}
