67a094fe046515a15a7c83f1c409f48c9f52d0be
Mario <mario@gielissen.org>
2021-03-26 11:45:31 +0100

    Invoice Treeview stat colors

---
+++ ow_invoice_tree_color/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Openworx
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+
+{
+    'name': 'Invoice Treeview state color',
+    'summary': 'Invoice Treeview state color',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': """
+Show colors on Invoice Treeview depending on state.
+""",
+    'author': "Openworx",
+    'website': 'https://www.openworx.nl',
+    'license': 'LGPL-3',
+    'images': ['images/screen.png'],
+    'depends': [
+	'account',
+    ],
+    'data': [
+        'views/invoice_tree.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
