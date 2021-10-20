4a4ccde63194fe7877fc529b5cc3797128984217
Mario Gielissen <mario@gielissen.org>
2021-10-20 21:02:05 +0200

    Sales & Purchase Order Paid Ribbon added

---
+++ ow_purchase_order_paid_ribbon/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Openworx
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Purchase Order Paid Ribbon',
+    'summary': 'Paid Ribbon on Purchase Order Form',
+    'version': '1.0',
+    'category': 'Purchases',
+    'summary': """
+Paid Ribbon on Purchase Order Form
+""",
+    'author': "Openworx",
+    'website': 'https://www.openworx.nl',
+    'license': 'LGPL-3',
+    'depends': [
+	'sale',
+    ],
+    'data': [
+        'views/purchase_order_form.xml',
+    ],
+    'images': ['images/image.png'],
+    'installable': True,
+    'application': False,
+}
---
+++ ow_sale_order_paid_ribbon/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Openworx
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Sales Order Paid Ribbon',
+    'summary': 'Paid Ribbon on Sales Order Form',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': """
+Paid Ribbon on Sales Order Form
+""",
+    'author': "Openworx",
+    'website': 'https://www.openworx.nl',
+    'license': 'LGPL-3',
+    'depends': [
+	'sale',
+    ],
+    'data': [
+        'views/sale_order_form.xml',
+    ],
+    'images': ['images/image.png'],
+    'installable': True,
+    'application': False,
+}
